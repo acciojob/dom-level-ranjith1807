39deb0049f439beddb198e704c91af19737ed307
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-03-30 14:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let level=document.getElementById("level");
+let domlevel=0;
+while(level){
+	domlevel++;
+	level=level.parentElement;
+}
+alert(`The level of the element is: ${domlevel});
